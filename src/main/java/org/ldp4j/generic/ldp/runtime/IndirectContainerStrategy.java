9a56a2b1750ae019d912f2f4b78d1f097e380911
Nandana Mihindukulasooriya <nandana.cse@gmail.com>
2015-01-30 23:36:36 +0100

    inital impl for indirect container POST

--- src/main/java/org/ldp4j/generic/ldp/runtime/IndirectContainerStrategy.java
+++ src/main/java/org/ldp4j/generic/ldp/runtime/IndirectContainerStrategy.java
@@ -2,12 +2,14 @@ package org.ldp4j.generic.ldp.runtime;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
-/**
- * Created by nandana on 1/27/15.
- */
-public class IndirectContainerStrategy implements ContainerStrategy {
+public class IndirectContainerStrategy extends DirectContainerStrategy {
     @Override
     public void addMemberTriple(String containerURI, String newURI, Model resource, Model container) {
 
+    }
+
+    @Override
+    protected void process(String containerURI, Model model) {
+        super.process(containerURI, model);
     }
 }
